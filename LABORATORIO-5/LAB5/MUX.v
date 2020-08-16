aa5d61d8ba1a45574d1a74b785659e021498cdd7
fon19722 <50242187+GAFong@users.noreply.github.com>
2020-08-16 10:05:16 -0600

    COMENTARIOS

--- LABORATORIO-5/LAB5/MUX.v
+++ LABORATORIO-5/LAB5/MUX.v
@@ -1,78 +1,80 @@
+//GABRIEL ALEXANDER FONG PENAGOS
+//CARNE 19722
+//ELECTRONICA DIGITAL
+//LABORATORIO 05
+// MUXES
+module MUX2_1(input wire A, B, C, output wire Y);                                // CEAMOS EL MODLO PARA EL MUX 2:1       A es el selector, B Y C SON INPUTS
 
-
-
-
-module MUX2_1(input wire A, B, C, output wire Y); //A es el selector, B Y C SON INPUTS
-
-  assign Y = A?C:B;
+  assign Y = A?C:B;                                                             //USAMOS EL OPERADOR TERNARIO ?, EL CUAL SELECCIONA QUIEN LE DARA EL VALOR A Y
 
 endmodule
 
-module MUX4_1(input wire  A, B, C, D, input wire[1:0]S, output wire Y); // S PUEDE SER 1 O 0, SE AGREGA EL VALOR ADENTRO DE CORCHETES
-
+module MUX4_1(input wire  A, B, C, D, input wire[1:0]S, output wire Y);         //CREAMOS EL MODULO PARA EL MUX 4:1, SE UTILIZAN 3 MUX 2:1 PARA CREARLO
+                                                                                //S PUEDE SER 1 O 0, SE AGREGA EL VALOR ADENTRO DE CORCHETESS,EL CUAL SELECCIONA LA POSICION DE LOS BITS
   wire OUT1, OUT2;
 
-  MUX2_1 mux1(S[0], A, B, OUT1);
-  MUX2_1 mux2(S[0], C, D, OUT2);
-  MUX2_1 mux3(S[1], OUT1, OUT2, Y);
+  MUX2_1 mux1(S[0], A, B, OUT1);                                                //UTILIZAMOS EL MUX 2:1 PARA SELECCIONAR CUAL DE LAS 2 ENTRADAS DARA EL VALOR DE SALIDA
+  MUX2_1 mux2(S[0], C, D, OUT2);                                                //UTILIZAMOS EL MUX 2:1 PARA SELECCIONAR CUAL DE LAS ULTIMAS 2 ENTRADAS DARA EL VALOR DE SALIDA
+  MUX2_1 mux3(S[1], OUT1, OUT2, Y);                                             //UTILIZAMOS EL MUX 2:1 PARA SELECCIONAR CUAL DE LOS DOS MUXES ANTERIORES DARA EL VALOR DE SALIDA
 
 endmodule
 
-module MUX8_1(input wire A, B, C, D, E, F, G, H, input wire [2:0] S, output wire Y); // S PUEDE SER 2, 1, 0
-
+module MUX8_1(input wire A, B, C, D, E, F, G, H, input wire [2:0] S, output wire Y); //CREAMOS EL MODULO PARA EL MUX 8:1 SE UTILIZAN 2 MUX 4:1 Y 1 MUX 2:1
+                                                                                    // S PUEDE SER 2, 1, 0
   wire OUT1, OUT2;
 
-  MUX4_1 mux1(A, B, C, D, S[1:0], OUT1);
-  MUX4_1 mux2(E, F, G, H, S[1:0], OUT2);
-  MUX2_1 mux3(S[2], OUT1, OUT2, Y);
+  MUX4_1 mux1(A, B, C, D, S[1:0], OUT1);                                        //UTILIZAMOS EL MUX 4:1 PARA SELECCIONAR CUAL DE LAS 4 ENTRADAS DARA EL VALOR DE SALIDA
+  MUX4_1 mux2(E, F, G, H, S[1:0], OUT2);                                        //UTILIZAMOS EL MUX 4:1 PARA SELECCIONAR CUAL DE LAS SIGUIENTES 4 ENTRADAS DARA EL VALOR DE SALIDA
+  MUX2_1 mux3(S[2], OUT1, OUT2, Y);                                             //UTILIZAMOS EL MUX 2:1 PARA SELECCIONAR CUAL DE LOS DOS MUXES 4:1 ANTERIORES DARA EL VALOR
+                                                                                //TENER EN CUENTA QUE COLOCAMOS QUE EN EL MUX 4:1, SE INGRESAN DE PRIMERO LAS 4 ENTRADAS Y DESPUES EL selector
+endmodule                                                                       //EN EL MUX 2:1 SE COLOCA DE PRIMERO EL SELECTOR Y LUEGO LAS 2 ENTRADAS
 
-endmodule
-
-module t1_mux2_1(input wire A, B, C, output wire Y); // TABLA 1 CON MUX 2:1 A ES SELECTOR
-
+module t1_mux2_1(input wire A, B, C, output wire Y);                            //CREAMOS EL MODULO TABLA 1 CON MUX 2:1 A ES SELECTOR
+                                                                                //A SERA NUESTRO SELECTOR.  B Y C SERAN LOS QUE DEFINAN EL VALOR DE SALIDA
   wire RES1, RES2;
-  assign RES1 = (B^C);                              //REALIZAMOS XOR PARA OBTENER LOS VALORES CUANDO A = 0
-  assign RES2 = ~(B^C);                             //REALIZAMOS XNOR PARA OBTENER LOS VALORES CUANDO A = 1
+  assign RES1 = (B^C);                                                          //REALIZAMOS XOR PARA OBTENER LOS VALORES CUANDO A = 0
+  assign RES2 = ~(B^C);                                                         //REALIZAMOS XNOR PARA OBTENER LOS VALORES CUANDO A = 1
 
-  MUX2_1 U1(A, RES1, RES2, Y);
+  MUX2_1 U1(A, RES1, RES2, Y);                                                  //UTILIZAMOS EL MUX 2:1 PARA TERMINAR DE REALIZAR LA TABLA 1
 
 endmodule
 
-module t1_mux4_1(input wire A, B, C, output wire Y);
+module t1_mux4_1(input wire A, B, C, output wire Y);                            //CREAMOS EL MODULO TABLA 1 CON MUX 4:1    A Y B SON SELECTORES
+                                                                                //A SERA NUESTRO SELECTOR DE LA SEGUNDA CIFRA Y B DE LA PRIMER CIFRA DE  Y C SERA EL QUE DEFINA EL VALOR DE SALIDA
   wire [1:0]S;
-  assign S[0] = B;
+  assign S[0] = B;                                                              //LE ASIGNAMOS LA POSICION A NUESTROS SELECTORES
   assign S[1] = A;
-  MUX4_1 U2(C, ~C, ~C, C,S[1:0], Y);
+  MUX4_1 U2(C, ~C, ~C, C,S[1:0], Y);                                            //UTILIZAMOS EL MUX 4:1 PARA TERMINAR DE REALIZAR LA TABLA 1
 endmodule
 
-module t1_mux8_1(input wire A, B, C, GND, VCC, output wire Y);
-  wire [2:0]S;
-  assign S[0] = C;
+module t1_mux8_1(input wire A, B, C, GND, VCC, output wire Y);                  //CREAMOS EL MODULO TABLA 1 CON MUX 8:1    A B C  SON SELECTORES
+  wire [2:0]S;                                                                  // VCC Y GND DEFINIRAN LOS VALORES DE SALIDA, SIENDO GND 0 Y VCC 1
+  assign S[0] = C;                                                              //ASIGNAMOS LAS POSICIONES DE LOS SELECTORES
   assign S[1] = B;
   assign S[2] = A;
-  MUX8_1 U3(GND, VCC, VCC, GND, VCC, GND, GND, VCC, S[2:0], Y);
+  MUX8_1 U3(GND, VCC, VCC, GND, VCC, GND, GND, VCC, S[2:0], Y);                 //UTILIZAMOS EL MUX 8:1 PARA TERMINAR DE REALIZAR LA TABLA 1
 endmodule
 
-module t2_mux2_1(input wire A, B, C, output wire Y);
-
+module t2_mux2_1(input wire A, B, C, output wire Y);                            //CREAMOS EL MODULO TABLA 2 CON MUX 2:1 A ES SELECTOR
+                                                                                //A SERA NUESTRO SELECTOR.  B Y C SERAN LOS QUE DEFINAN EL VALOR DE SALIDA
   wire RES1, RES2;
-  assign RES1 = ~(B | C);
-  assign RES2 = ~(B & C);
-  MUX2_1 U3(A, RES1, RES2, Y);
+  assign RES1 = ~(B | C);                                                       //REALIZAMOS NOR PARA OBTENER LOS VALORES CUANDO A = 0
+  assign RES2 = ~(B & C);                                                       //REALIZAMOS NAND PARA OBTENER LOS VALORES CUANDO A = 1
+  MUX2_1 U3(A, RES1, RES2, Y);                                                  //UTILIZAMOS EL MUX 2:1 PARA TERMINAR DE REALIZAR LA TABLA 2
 endmodule
 
-module t2_mux4_1(input wire A, B, C, GND, output wire Y);
-
+module t2_mux4_1(input wire A, B, C, GND, output wire Y);                       //CREAMOS EL MODULO TABLA 2 CON MUX 4:1    A Y B SON SELECTORES
+                                                                                //A SERA NUESTRO SELECTOR DE LA SEGUNDA CIFRA Y B DE LA PRIMER CIFRA DE  Y C Y GND SERAN LOS QUE DEFINA EL VALOR DE SALIDA
   wire [1:0]S;
-  assign S[0] = B;
+  assign S[0] = B;                                                              //ASIGNAMOS LA POSISICION A NUESTROS SELECTORES
   assign S[1] = A;
-  MUX4_1 U5(~C, GND, C, ~C, S[1:0], Y);
+  MUX4_1 U5(~C, GND, C, ~C, S[1:0], Y);                                         //UTILIZAMOS EL MUX 4:1 PARA TERMINAR DE REALIZAR LA TABLA 2
 endmodule
 
-module t2_mux8_1(input wire A, B, C, GND, VCC, output wire Y);
-  wire [2:0]S;
-  assign S[0] = C;
+module t2_mux8_1(input wire A, B, C, GND, VCC, output wire Y);                  //CREAMOS EL MODULO TABLA 1 CON MUX 8:1    A B C  SON SELECTORES
+  wire [2:0]S;                                                                  // VCC Y GND DEFINIRAN LOS VALORES DE SALIDA, SIENDO GND 0 Y VCC 1
+  assign S[0] = C;                                                              //ASIGNAMOS LAS POSICIONES DE LOS SELECTORES
   assign S[1] = B;
   assign S[2] = A;
-  MUX8_1 U6(VCC, GND, GND, GND, GND, VCC, VCC, GND, S[2:0], Y);
+  MUX8_1 U6(VCC, GND, GND, GND, GND, VCC, VCC, GND, S[2:0], Y);                 //UTILIZAMOS EL MUX 8:1 PARA TERMINAR DE REALIZAR LA TABLA 2
 endmodule
--- LABORATORIO-5/LAB5/MUX_tb.v
+++ LABORATORIO-5/LAB5/MUX_tb.v
@@ -1,28 +1,32 @@
-
+//GABRIEL ALEXANDER FONG PENAGOS
+//CARNE 19722
+//ELECTRONICA DIGITAL
+//LABORATORIO 05
+//MUXES
 
 module testbench ();
 
-  wire GND;
-  wire VCC;
+  wire GND;                                                                     //GND COMO CABLE Y LUEGO LE ASIGNAMOS DE VALOR 0
+  wire VCC;                                                                     //VCC COMO GABLE Y LUEGO LE ASIGNAMOS DE VALOR 1
   assign GND = 0;
   assign VCC = 1;
-  reg s1, in1B, in1C;
-  reg s2, s3, in2C;
-  reg s4, s5, s6;
-  reg s7, in4B, in4C;
-  reg s8, s9, in5C;
-  reg s10, s11, s12;
-  wire out1, out2, out3, out4, out5, out6;
+  reg s1, in1B, in1C;                                                           //CREAMOS LOS REGISTROS PARA t1_mux2_1
+  reg s2, s3, in2C;                                                             //CREAMOS LOS REGISTROS PARA t1_mux4_1
+  reg s4, s5, s6;                                                               //CREAMOS LOS REGISTROS PARA t1_mux8_1
+  reg s7, in4B, in4C;                                                           //CREAMOS LOS REGISTROS PARA t2_mux2_1
+  reg s8, s9, in5C;                                                             //CREAMOS LOS REGISTROS PARA t2_mux4_1
+  reg s10, s11, s12;                                                            //CREAMOS LOS REGISTROS PARA t2_mux8_1
+  wire out1, out2, out3, out4, out5, out6;                                      //CREAMOS LAS SALIDAS PARA LOS MODULOS
 
-  t1_mux2_1 U1(s1, in1B, in1C, out1);
-  t1_mux4_1 U2(s2, s3, in2C, out2);
-  t1_mux8_1 U3(s4, s5, s6, GND, VCC, out3);
-  t2_mux2_1 U4(s7, in4B, in4C, out4);
-  t2_mux4_1 U5(s8, s9, in5C, GND, out5);
-  t2_mux8_1 U6(s10, s11, s12, GND, VCC, out6);
+  t1_mux2_1 U1(s1, in1B, in1C, out1);                                           //s1 ES NUESTRO SELECTOR (A), in1B, in1C son los valores de input de B y C
+  t1_mux4_1 U2(s2, s3, in2C, out2);                                             //s2, s3 SON NUESTROS SELECTORES (A), (B). in2c el valor de input de C
+  t1_mux8_1 U3(s4, s5, s6, GND, VCC, out3);                                     //s4, s5, s6 SON NUESTROS SELECTORES (A), (B), (C)
+  t2_mux2_1 U4(s7, in4B, in4C, out4);                                           //s7 ES NUESTRO SELECTOR (A), in4B, in4C son los valores de input de B y C
+  t2_mux4_1 U5(s8, s9, in5C, GND, out5);                                        //s8, s9 SON NUESTROS SELECTORES (A), (B). in5c el valor de input de C
+  t2_mux8_1 U6(s10, s11, s12, GND, VCC, out6);                                  //s10, s11, s12 SON NUESTROS SELECTORES (A), (B), (C)
 
 
-  initial begin
+  initial begin                                                                 //INICIAMOS LA TABLA 1 CON EL MUX 2:1
   #1
   $display("\n");
   $display(" TABLA 1 MUX 2:1 ");
@@ -40,7 +44,7 @@ module testbench ();
   #1 s1 = 1; in1B = 1; in1C = 1;
   end
 
-  initial begin
+  initial begin                                                                 //INICIAMOS LA TABLA 1 CON EL MUX 4:1
   #9
   $display("\n");
   $display(" TABLA 1 MUX 4:1 ");
@@ -58,7 +62,7 @@ module testbench ();
   #1 s2 = 1; s3 = 1; in2C = 1;
   end
 
-  initial begin
+  initial begin                                                                 //INICIAMOS LA TABLA 1 CON EL MUX 8:1
   #17
   $display("\n");
   $display(" TABLA 1 MUX 8:1 ");
@@ -76,7 +80,7 @@ module testbench ();
   #1 s4 = 1; s5 = 1; s6 = 1;
   end
 
-  initial begin
+  initial begin                                                                 //INICIAMOS LA TABLA 2 CON EL MUX 2:1
   #25
   $display("\n");
   $display(" TABLA 2 MUX 2:1 ");
@@ -94,7 +98,7 @@ module testbench ();
   #1 s7 = 1; in4B = 1; in4C = 1;
   end
 
-  initial begin
+  initial begin                                                                  //INICIAMOS LA TABLA 2 CON EL MUX 4:1
   #33
   $display("\n");
   $display(" TABLA 2 MUX 4:1 ");
@@ -112,7 +116,7 @@ module testbench ();
   #1 s8 = 1; s9 = 1; in5C = 1;
   end
 
-  initial begin
+  initial begin                                                                 //INICIAMOS LA TABLA 2 CON EL MUX 8:1
   #43
   $display("\n");
   $display(" TABLA 1 MUX 8:1 ");
@@ -130,7 +134,7 @@ module testbench ();
   #1 s10 = 1; s11 = 1; s12 = 1;
   end
 
-  initial begin                                                               //INICIAMOS PARA COLOCAR LOS DATOS EN GTK WAVE
+  initial begin                                                                 //INICIAMOS PARA COLOCAR LOS DATOS EN GTK WAVE
         $dumpfile("MUX_tb.vcd");
         $dumpvars(0, testbench);
       end
